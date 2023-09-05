0537f8b836cc6e0ec21cc1e391b065e8c4526a64
KshitijaTondlekar <kshitijatondlekar9@gmail.com>
2023-09-05 14:27:33 +0530

    Pattern Program

--- Pattern Program/P1.java
+++ Pattern Program/P1.java
@@ -1,16 +1,21 @@
 package com.jsp.patterns;
 
-public class P1 {
-	public static void main(String[] args) {
-		int n= 5;
-		for(int i = 1; i<=n; i++) {
-			for(int s=1; s<=n-i;s++) {
-				System.out.print(" ");			
-				}
-			for(int j=1;j<=i;j++) {
-				System.out.print(j);
+public class Prog {
+	public static void main(String args[]) {
+	int n=5;
+	for (int i=5;i>=1;i--) {
+		for(int s=1;s<=i;s++) {
+			System.out.print(" ");
+		}
+			for(int j=0;j<=n-i;j++) {
+				System.out.print("*");
+				
+				
 			}
 			System.out.println();
+			}
 		}
-	}
 }
+
+
+
